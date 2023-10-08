604cb9119f94e76a083d7ffced001c3f1ea09a61
kes-knows-nothing <allinpositive@gmail.com>
2023-10-08 22:23:50 +0900

    Step 2 매트릭스 선택

--- js/tetris.js
+++ js/tetris.js
@@ -1,14 +1,67 @@
 const playground = document.querySelector(".playground > ul");
 
-console.log(playground);
+// Settubg
+const GAME_ROWS = 20;
+const GAME_COLS = 10;
 
-for (let i = 0; i < 20; i++) {
+// variables
+
+let score = 0;
+let duration = 0;
+let downInterval;
+let tempMovingItem;
+
+const BLOCKS = {
+  tree: [
+    [
+      [0, 0],
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ],
+    [],
+    [],
+    [],
+  ],
+};
+
+const movingItem = {
+  type: "tree",
+  direction: 0,
+  top: 0,
+  left: 0,
+};
+
+// functions
+
+const init = () => {
+  tempMovingItem = { ...movingItem };
+  for (let i = 0; i < GAME_ROWS; i++) {
+    perpendNewLine();
+  }
+  renderingBlock();
+};
+
+const perpendNewLine = () => {
   const li = document.createElement("li");
   const ul = document.createElement("ul");
-  for (let j = 0; j < 10; j++) {
+  for (let j = 0; j < GAME_COLS; j++) {
     const matrix = document.createElement("li");
     ul.prepend(matrix);
   }
   li.prepend(ul);
   playground.prepend(li);
-}
+};
+
+const renderingBlock = () => {
+  const { type, direction, top, left } = tempMovingItem;
+  console.log(type, direction, top, left);
+  BLOCKS[type][direction].forEach((block) => {
+    const x = block[0];
+    const y = block[1];
+    const target = playground.childNodes[y].childNodes[0].childNodes[x];
+    console.log(target);
+  });
+};
+
+init();
